9e1051ff737c4ecf43b0dec14aad651699924e83
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2016-07-06 19:58:58 +0100

    Restructured src folder to have a folder to Scala, Python and R examples

--- src/scala/src/main/scala/com/jorgefigueiredo/job/Main.scala
+++ src/scala/src/main/scala/com/jorgefigueiredo/job/Main.scala
@@ -1,22 +1,26 @@
 package com.jorgefigueiredo.job
 
 import org.apache.spark.{SparkConf, SparkContext}
-
+import sys.process._
 
 object Main {
 
   def main(args: Array[String]): Unit = {
 
+    println("------------ Starting ---------------")
+
+    "printenv" !
+
     val sparkConf = new SparkConf()
     sparkConf.setAppName("Spark Job")
-    sparkConf.setMaster("local[4]")
+    sparkConf.setMaster("local[*]")
 
     val sparkContext = new SparkContext(sparkConf)
     val list = sparkContext.parallelize(1 to 1000, 1)
     val filter = list.filter(item => item % 2 == 0)
     val count = filter.count
 
-    println(count)
+    println("RESULT: " + count)
   }
 
 }
