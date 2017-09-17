43f8e24c75e25d9157717a5ade2a29ed380791e4
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2017-09-17 15:42:13 +0100

    Trying to submit coverage results

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,5 @@ cd src/scala;
 wget https://github.com/codacy/codacy-coverage-reporter/releases/download/2.0.0/codacy-coverage-reporter-2.0.0-assembly.jar
 
 mvn clean test cobertura:cobertura coveralls:report;
+
+java -cp codacy-coverage-reporter-assembly-2.0.0.jar com.codacy.CodacyCoverageReporter -l Java -r target/site/cobertura/coverage.xml
